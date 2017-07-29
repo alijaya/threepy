f46e57f232392ef491c7f73c1a20ce1d8c39db58
Ali Jaya Meilio <alijayameilio@gmail.com>
2017-07-29 03:06:11 +0200

    update jstopy

--- tools/jstopy.py
+++ tools/jstopy.py
@@ -30,6 +30,7 @@ rules = [
     ( "Math.round", "round" ),
     ( "Math.max", "max" ),
     ( "Math.min", "min" ),
+    ( "Math.PI", "math.pi" ),
     ( "Math", "math" ),
     ( "\+\+", "+= 1" ),
     ( " = (.*) \? (.*) : ([^\n]*)", " = \g<2> if \g<1> else \g<3>" ),
@@ -38,6 +39,7 @@ rules = [
 ]
 
 rulesClass = [
+    ( "[^\n]*\.prototype = ", "" ),
     ( "Object\.assign\( [^\n]*\n", "" ),
     ( "([\S]*): function \((.*)\) {", "def \g<1>( self,\g<2>):" ),
     ( "function (.*)\((.*)\) {", "def \g<1>( self,\g<2>):" ),
@@ -48,10 +50,10 @@ rulesTest = [
     ( "QUnit\.module\( \"(.*)\" \)", "class Test\g<1>( unittest.TestCase ):" ),
     ( "QUnit\.test\( \"(.*)\"[^\n]*", "def test_\g<1>( self ):\n" ),
     ( "(\S*) = function\((.*)\) {", "def \g<1>(\g<2>):\n" ),
-    ( "assert\.ok\( (.*) == (.*), \"(.*)\" \)", "self.assertEqual( \g<1>, \g<2> ) # \g<3>" ),
-    ( "assert\.ok\( (.*) == (.*) \)", "self.assertEqual( \g<1>, \g<2> )" ),
-    ( "assert\.ok\( (.*), \"(.*)\" \)", "self.assertTrue( \g<1> ) # \g<2>" ),
-    ( "assert\.ok\( (.*) \)", "self.assertTrue( \g<1> )" )
+    ( "assert\.ok\( (.*) == (.*)", "self.assertEqual( \g<1>, \g<2>" ),
+    ( "assert\.ok\( (.*)", "self.assertTrue( \g<1>" ),
+    ( "assert.numEqual\( (.*), (.*)", "self.assertAlmostEqual( \g<1>, \g<2>" ),
+    ( ", \"(.*)\" \)", " ) # \g<1>" )
 ]
 
 rulesClean = [
@@ -60,15 +62,25 @@ rulesClean = [
     ( "\n\n$", "\n" )
 ]
 
+ruleClass = "import { (.*) } from '(.*)';"
+
 jspath = sys.argv[1]
-pypath = sys.argv[2]
-test = len( sys.argv ) > 3
+pypath = re.sub( "\.js", ".py", jspath )
+test = len( sys.argv ) > 2
 
 jsfile = open( jspath, "r" )
 pyfile = open( pypath, "w" )
 
 jstext = jsfile.read()
 pytext = jstext
+
+classes = []
+
+for m in re.finditer( ruleClass, pytext ):
+
+    temp = list( m.groups() )
+    temp.append( temp[0][0].lower() + temp[0][1:] )
+    classes.append( temp )
 
 for rule in rules:
 
@@ -79,20 +91,38 @@ if not test:
     for rule in rulesClass:
 
         pytext = re.sub( rule[0], rule[1], pytext )
+
+    for c in classes:
+
+        bef = " " + c[0]
+        aft = " " + c[2] + "." + c[0]
+        pytext = re.sub( bef, aft, pytext )
+
+    importStatement = ""
+
+    for c in classes:
+
+        s = c[1]
+        s = re.sub( "\.\.\/", ".", s )
+        s = re.sub( "^(\.*)", "\g<1>.", s)
+        s = re.sub( "\/", ".", s )
+        s = re.sub( c[0], c[2], s )
+        importStatement += "from %s import %s\n" % ( s, c[0] )
+
+    pytext = importStatement + pytext
     
 else:
 
     for rule in rulesTest:
 
         pytext = re.sub( rule[0], rule[1], pytext )
+    
+
+    pytext = "from __future__ import division\n\nimport unittest\n\nimport THREE\n\n" + pytext
 
 for rule in rulesClean:
 
     pytext = re.sub( rule[0], rule[1], pytext )
-
-if test:
-
-    pytext = "from __future__ import division\n\nimport unittest\n\nimport THREE\n\n" + pytext
 
 pyfile.write( pytext )
 
