bcdd3d5dafb7d585f00f4b0d4c7b11f71cacf737
Ali Jaya Meilio <alijayameilio@gmail.com>
2017-07-30 19:37:10 +0200

    testing

--- lie.py
+++ lie.py
@@ -7,3 +7,10 @@ height = 600
 
 scene = THREE.Scene()
 camera = THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 )
+
+geometry = THREE.BoxGeometry( 1, 1, 1 );
+# material = THREE.MeshBasicMaterial( { "color": 0x00ff00 } );
+# cube = THREE.Mesh( geometry, material );
+# scene.add( cube );
+
+# camera.position.z = 5;
